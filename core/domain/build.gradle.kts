9d3d147f9c7d92182202e36a1316ff6da48349c9
junseo511 <prime54@g.skku.edu>
2024-07-02 13:52:01 +0900

    build: Edit domain module dependencies

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,12 +1,9 @@
-import com.teamoffroad.app.setNamespace
-
 plugins {
-    id("offroad.android.library")
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
-android {
-    setNamespace("core.domain")
-}
-
-dependencies {
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
