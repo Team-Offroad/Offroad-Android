731ef4cf41b3c18bafbc3fbd7982e6e2aad3287f
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-07-06 06:24:33 +0900

    feature: Add mypage navigation

---
+++ feature/mypage/src/main/java/com/teamoffroad/feature/mypage/MypageScreen.kt
@@ -0,0 +1,27 @@
+package com.teamoffroad.feature.mypage
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+internal fun MypageRoute(
+    padding: PaddingValues,
+) {
+    MypageScreen(
+        padding = padding,
+    )
+}
+
+@Composable
+internal fun MypageScreen(
+    padding: PaddingValues,
+) {
+    Text(
+        "mypage screen",
+        fontSize = 40.sp,
+        textAlign = TextAlign.Center
+    )
+}
