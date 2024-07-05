ac2c75db4e6316301cd07bea31344df0451b3e4d
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-07-06 06:13:20 +0900

    feature: Add setting bottom bar

---
+++ feature/main/src/main/java/com/teamoffroad/feature/main/MainScreen.kt
@@ -0,0 +1,49 @@
+package com.teamoffroad.feature.main
+
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.teamoffroad.feature.main.component.MainBottomBar
+import com.teamoffroad.feature.main.component.MainNavHost
+import kotlinx.collections.immutable.toPersistentList
+
+@Composable
+internal fun MainScreen(
+    navigator: MainNavigator = rememberMainNavigator(),
+    modifier: Modifier = Modifier
+) {
+    MainScreenContent(
+        navigator = navigator,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun MainScreenContent(
+    modifier: Modifier = Modifier,
+    navigator: MainNavigator,
+) {
+    Scaffold(
+        modifier = modifier,
+        content = { padding ->
+            MainNavHost(
+                navigator = navigator,
+                padding = padding,
+            )
+        },
+        bottomBar = {
+            MainBottomBar(
+                modifier = Modifier
+                    .navigationBarsPadding()
+                    .padding(start = 8.dp, end = 8.dp, bottom = 28.dp),
+                visible = navigator.shouldShowBottomBar(),
+                tabs = MainTab.entries.toPersistentList(),
+                currentTab = navigator.currentTab,
+                onTabSelected = { navigator.navigate(it) }
+            )
+        },
+    )
+}
---
+++ feature/main/src/main/java/com/teamoffroad/feature/main/component/MainBottomBar.kt
@@ -0,0 +1,96 @@
+package com.teamoffroad.feature.main.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamoffroad.core.designsystem.theme.OffroadTheme
+import com.teamoffroad.feature.main.MainTab
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
+
+@Composable
+internal fun MainBottomBar(
+    modifier: Modifier = Modifier,
+    visible: Boolean,
+    tabs: PersistentList<MainTab>,
+    currentTab: MainTab?,
+    onTabSelected: (MainTab) -> Unit,
+) {
+    AnimatedVisibility(
+        visible = visible,
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .padding(horizontal = 28.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            tabs.forEach { tab ->
+                MainBottomBarItem(
+                    tab = tab,
+                    selected = tab == currentTab,
+                    onClick = { onTabSelected(tab) },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RowScope.MainBottomBarItem(
+    modifier: Modifier = Modifier,
+    tab: MainTab,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .selectable(
+                selected = selected,
+                indication = null,
+                role = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onClick,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            painter = painterResource(tab.iconResId),
+            contentDescription = tab.contentDescription,
+            modifier = Modifier.size(34.dp),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MainBottomBarPreview() {
+    OffroadTheme {
+        MainBottomBar(
+            visible = true,
+            tabs = MainTab.entries.toPersistentList(),
+            currentTab = MainTab.HOME,
+            onTabSelected = { },
+        )
+    }
+}
