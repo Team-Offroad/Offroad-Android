e292ed8c4c05212bde00dd9b4bf29b4287c9fc5a
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-07-06 06:15:29 +0900

    feature: Add home navigation

---
+++ feature/home/src/main/java/com/teamoffroad/feature/home/HomeScreen.kt
@@ -0,0 +1,27 @@
+package com.teamoffroad.feature.home
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+internal fun HomeRoute(
+    padding: PaddingValues,
+) {
+    HomeScreen(
+        padding = padding,
+    )
+}
+
+@Composable
+internal fun HomeScreen(
+    padding: PaddingValues,
+) {
+    Text(
+        "home screen",
+        fontSize = 40.sp,
+        textAlign = TextAlign.Center
+    )
+}
---
+++ feature/home/src/main/java/com/teamoffroad/feature/home/navigation/HomeNavigation.kt
@@ -0,0 +1,22 @@
+package com.teamoffroad.feature.home.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.teamoffroad.core.navigation.MainTabRoute
+import com.teamoffroad.feature.home.HomeRoute
+
+fun NavController.navigateHome(navOptions: NavOptions) {
+    navigate(MainTabRoute.Home, navOptions)
+}
+
+fun NavGraphBuilder.homeNavGraph(
+    padding: PaddingValues,
+) {
+    composable<MainTabRoute.Home> {
+        HomeRoute(padding)
+    }
+}
+
