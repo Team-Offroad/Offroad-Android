9df683141b2b78d6a5f59757a055b8411c62b525
YuJeongHyun <0703olivia@naver.com>
2024-07-10 19:30:15 +0900

    feature: Add popup tag inactive component

--- core/common/src/main/java/com/teamoffroad/core/common/component/PopupTag.kt
+++ core/common/src/main/java/com/teamoffroad/core/common/component/PopupTag.kt
@@ -1,6 +1,7 @@
 package com.teamoffroad.core.common.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,6 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.teamoffroad.core.designsystem.theme.Main2
+import com.teamoffroad.core.designsystem.theme.NametagInactive
+import com.teamoffroad.core.designsystem.theme.NametagStroke
 import com.teamoffroad.core.designsystem.theme.OffroadTheme
 import com.teamoffroad.core.designsystem.theme.Sub
 import com.teamoffroad.core.designsystem.theme.White
@@ -31,6 +35,23 @@ fun PopupTagActive(text: String) {
 }
 
 @Composable
-fun PopupTagInactive() {
-
+fun PopupTagInactive(text: String) {
+    Text(
+        text = text,
+        textAlign = TextAlign.Center,
+        color = Main2,
+        style = OffroadTheme.typography.subtitle2Semibold,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = NametagInactive,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .border(
+                width = 1.dp,
+                shape = RoundedCornerShape(10.dp),
+                color = NametagStroke
+            )
+            .padding(vertical = 8.dp)
+    )
 }
