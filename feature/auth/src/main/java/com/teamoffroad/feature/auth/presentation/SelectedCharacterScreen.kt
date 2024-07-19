4582541e5fe48255b3aefbafd66ebd71a9dc49d7
YuJeongHyun <0703olivia@naver.com>
2024-07-19 22:51:59 +0900

    feature: Add character svg

--- feature/auth/src/main/java/com/teamoffroad/feature/auth/presentation/SelectedCharacterScreen.kt
+++ feature/auth/src/main/java/com/teamoffroad/feature/auth/presentation/SelectedCharacterScreen.kt
@@ -1,5 +1,6 @@
 package com.teamoffroad.feature.auth.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,45 +8,90 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
 import com.teamoffroad.core.designsystem.component.OffroadActionBar
 import com.teamoffroad.core.designsystem.theme.OffroadTheme
 import com.teamoffroad.core.designsystem.theme.SelectedCharacterBackground
 import com.teamoffroad.core.designsystem.theme.SelectedCharacterBottom
 import com.teamoffroad.core.designsystem.theme.SelectedCharacterText
 import com.teamoffroad.feature.auth.presentation.component.OffroadBasicBtn
+import com.teamoffroad.offroad.feature.auth.R
 
 @Composable
 internal fun SelectedCharacterScreen(
     selectedCharacterUrl: String,
     navigateToHome: () -> Unit,
 ) {
-    Box {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(SelectedCharacterBackground),
-        ) {
-            OffroadActionBar(SelectedCharacterBackground)
-            Box {
+    OffroadTheme {
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(SelectedCharacterBackground),
+            ) {
+                OffroadActionBar(SelectedCharacterBackground)
+                Spacer(modifier = Modifier.padding(270.dp))
                 Column(
                     modifier = Modifier
+                        .weight(1f)
+                        .background(SelectedCharacterBottom)
                         .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Spacer(modifier = Modifier.height(118.dp))
-                    Text(
+                    Spacer(
+                        modifier = Modifier.weight(1f)
+                    )
+                    OffroadBasicBtn(
                         text =
-                        "프로필 생성을 축하드려요!",
+                        "모험 시작하기",
+                        isActive = true,
+                        modifier = Modifier
+                            .padding(bottom = 24.dp)
+                            .width(312.dp)
+                            .height(50.dp),
+                        onClick = navigateToHome
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .padding(bottom = 116.dp)
+                    .align(Alignment.BottomCenter)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.bg_auth_selected_character_light),
+                    contentDescription = ""
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 118.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        text = "프로필 생성을 축하드려요!",
                         fontSize = 22.sp,
                         color = SelectedCharacterText,
-                        style = OffroadTheme.typography.title
+                        style = OffroadTheme.typography.title,
                     )
                     Text(
                         text = "지금 바로 모험을 떠나볼까요?",
@@ -53,36 +99,23 @@ internal fun SelectedCharacterScreen(
                         color = SelectedCharacterText,
                         style = OffroadTheme.typography.title
                     )
-                    Spacer(modifier = Modifier.height(330.dp))
                 }
             }
-            Column(
+
+            Box(
                 modifier = Modifier
-                    .background(SelectedCharacterBottom)
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
+                    .fillMaxWidth()
+                    .padding(bottom = 134.dp),
+                contentAlignment = Alignment.BottomCenter
             ) {
-                Spacer(
-                    modifier = Modifier.height(
-                        162.dp
-                    )
-                )
-                OffroadBasicBtn(
-                    text =
-                    "모험 시작하기",
-                    isActive = true,
-                    modifier = Modifier
-                        .width(312.dp)
-                        .height(50.dp),
-                    onClick = navigateToHome
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(selectedCharacterUrl)
+                        .decoderFactory(SvgDecoder.Factory())
+                        .build(),
+                    contentDescription = "character"
                 )
             }
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .background(SelectedCharacterBottom)
-            )
         }
     }
 }
