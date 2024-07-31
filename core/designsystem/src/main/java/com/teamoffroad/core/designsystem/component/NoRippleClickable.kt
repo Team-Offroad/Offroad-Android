ba1b454e11d7d2e91730a858669c322ea3c2c83b
junseo511 <prime54@g.skku.edu>
2024-07-31 09:09:14 +0900

    refactor: Remove unused import

--- core/designsystem/src/main/java/com/teamoffroad/core/designsystem/component/NoRippleClickable.kt
+++ core/designsystem/src/main/java/com/teamoffroad/core/designsystem/component/NoRippleClickable.kt
@@ -1,23 +1,16 @@
 package com.teamoffroad.core.designsystem.component
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 
 fun Modifier.clickableWithoutRipple(
     interactionSource: MutableInteractionSource,
-    onClick: () -> Unit
-) = composed(
-    factory = {
-        this.then(
-            Modifier.clickable(
-                interactionSource = interactionSource,
-                indication = null,
-                onClick = { onClick() }
-            )
-        )
-    }
-)
+    onClick: () -> Unit,
+): Modifier {
+    return this.clickable(
+        interactionSource = interactionSource,
+        indication = null,
+        onClick = onClick
+    )
+}
