b0ea038480fc991245ecc6ccf8c808fe2ba5fc8c
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-09-05 00:56:27 +0900

    feature: Add emblems api

--- feature/mypage/src/main/java/com/teamoffroad/feature/mypage/presentation/GainedEmblemsScreen.kt
+++ feature/mypage/src/main/java/com/teamoffroad/feature/mypage/presentation/GainedEmblemsScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -18,6 +20,7 @@ import com.teamoffroad.core.designsystem.theme.Gray100
 import com.teamoffroad.core.designsystem.theme.Main1
 import com.teamoffroad.feature.mypage.presentation.component.GainedEmblemsHeader
 import com.teamoffroad.feature.mypage.presentation.component.GainedEmblemsItems
+import com.teamoffroad.feature.mypage.presentation.model.GainedEmblemsResult
 import com.teamoffroad.offroad.feature.mypage.R
 
 @SuppressLint("StateFlowValueCalledInComposition")
@@ -26,6 +29,8 @@ internal fun GainedEmblemsScreen(
     navigateToBack: () -> Unit,
     viewModel: GainedEmblemsViewModel = hiltViewModel(),
 ) {
+    val isEmblemState by viewModel.emblemsUiState.collectAsState()
+
     LaunchedEffect(Unit) {
         viewModel.getEmblems()
     }
@@ -44,6 +49,8 @@ internal fun GainedEmblemsScreen(
         Spacer(modifier = Modifier
             .height(1.dp)
             .background(color = Gray100))
-        viewModel.gainedEmblems.value?.let { GainedEmblemsItems(gainedEmblemsList = it) }
+        if (isEmblemState.nicknameValidateResult == GainedEmblemsResult.Success){
+            GainedEmblemsItems(isEmblemState = isEmblemState)
+        }
     }
 }
