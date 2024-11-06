72f86184e8c2d9ca7f7aa22f18d16448362b59e8
YuJeongHyun <0703olivia@naver.com>
2024-11-07 00:38:25 +0900

    refactor: Delete svg dependencies

--- feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/upload/UploadImage.kt
+++ feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/upload/UploadImage.kt
@@ -3,9 +3,7 @@ package com.teamoffroad.feature.home.presentation.component.upload
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.PictureDrawable
 import android.net.Uri
 import android.os.Environment
 import android.util.Log
@@ -16,18 +14,11 @@ import coil.decode.SvgDecoder
 import coil.request.ErrorResult
 import coil.request.ImageRequest
 import coil.request.SuccessResult
-import com.caverock.androidsvg.SVG
-import com.teamoffroad.feature.home.presentation.component.download.downloadImage
-import com.teamoffroad.offroad.core.common.BuildConfig
 import com.teamoffroad.offroad.feature.home.R
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 suspend fun uploadImage(context: Context, imageUrl: String) {
     val uri = downloadAndConvertImage(context, imageUrl)
