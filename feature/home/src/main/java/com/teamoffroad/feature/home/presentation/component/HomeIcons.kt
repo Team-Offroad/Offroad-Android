76870748845749fc23760d5720565bc7d1b3290b
YuJeongHyun <0703olivia@naver.com>
2024-11-14 17:46:00 +0900

    refactor: Add home chat exist

--- feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/HomeIcons.kt
+++ feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/HomeIcons.kt
@@ -1,26 +1,25 @@
 package com.teamoffroad.feature.home.presentation.component
 
-import android.Manifest
 import android.content.Context
 import android.os.Build
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.teamoffroad.core.designsystem.component.clickableWithoutRipple
+import com.teamoffroad.core.designsystem.theme.ErrorNew
 import com.teamoffroad.offroad.feature.home.R
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -30,43 +29,33 @@ fun HomeIcons(
     imageUrl: String,
     navigateToGainedCharacter: () -> Unit,
 ) {
-    val scope = rememberCoroutineScope()
-    val permission = Manifest.permission.WRITE_EXTERNAL_STORAGE
-
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            Toast.makeText(context, "권한이 허용되었습니다.", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(context, "권한이 허용되지 않았습니다.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
     Box(
         contentAlignment = Alignment.TopEnd,
-        modifier = Modifier.aspectRatio(48f / 144f).padding(top = 80.dp, end = 20.dp)
+        modifier = Modifier.padding(top = 80.dp, end = 20.dp)
     ) {
-        Column {
-            val downloadInteractionSource = remember { MutableInteractionSource() }
-            Image(
-                painter = painterResource(id = R.drawable.ic_home_chat),
-                contentDescription = "chat",
-                modifier = Modifier
-//                modifier = Modifier
-//                    .clickableWithoutRipple(downloadInteractionSource) {
-//                        if (ContextCompat.checkSelfPermission(
-//                                context,
-//                                permission
-//                            ) == PackageManager.PERMISSION_GRANTED
-//                        ) {
-//                            downloadImage(context, imageUrl, scope)
-//                            Toast.makeText(context, "이미지 다운 완료", Toast.LENGTH_SHORT).show()
-//                        } else {
-//                            launcher.launch(permission)
-//                        }
-//                    }
-            )
+        Column(modifier = Modifier.width(48.dp)) {
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_home_chat),
+                    contentDescription = "chat",
+                )
+
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.TopEnd
+                ) {
+                    Canvas(
+                        modifier = Modifier
+                            .padding(top = 6.dp, end = 6.dp)
+                            .size(8.dp)
+                    ) {
+                        drawCircle(
+                            color = ErrorNew,
+                            style = Fill
+                        )
+                    }
+                }
+            }
 
             Image(
                 painter = painterResource(id = R.drawable.ic_home_upload),
