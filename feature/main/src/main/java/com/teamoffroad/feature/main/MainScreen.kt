d51b52eff6af920d3bf41b1ebbf343081dd95ab0
junseo511 <prime54@g.skku.edu>
2024-11-17 01:35:04 +0900

    feature: Add CharacterChat background

--- feature/main/src/main/java/com/teamoffroad/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/teamoffroad/feature/main/MainScreen.kt
@@ -2,9 +2,6 @@ package com.teamoffroad.feature.main
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -37,7 +34,7 @@ private fun MainScreenContent(
             MainNavHost(
                 navigator = navigator,
                 padding = padding,
-                modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars),
+                modifier = Modifier,
             )
             OnBackButtonListener(
                 navigator::popBackStackIfNotMainTabRoute,
