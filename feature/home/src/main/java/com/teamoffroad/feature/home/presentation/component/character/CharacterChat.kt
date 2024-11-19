93faceade1dd64c5fbf1d961286ff14d468a1efc
YuJeongHyun <0703olivia@naver.com>
2024-11-19 17:15:40 +0900

    refactor: Add home chat answer string.xml

--- feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/character/CharacterChat.kt
+++ feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/character/CharacterChat.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -163,7 +164,7 @@ fun AnswerCharacterChat(
     ) {
         Box(contentAlignment = Alignment.CenterEnd, modifier = Modifier.fillMaxWidth()) {
             Text(
-                text = "답장하기",
+                text = stringResource(id = R.string.home_chat_answer),
                 modifier = Modifier
                     .background(
                         color = backgroundColor,
