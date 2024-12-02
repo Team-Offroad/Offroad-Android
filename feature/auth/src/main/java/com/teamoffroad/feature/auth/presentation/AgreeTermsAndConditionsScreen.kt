a012d265565547eed05890753ae3458ef0819268
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-12-02 10:41:07 +0900

    refactor: Edit snackbar message

--- feature/auth/src/main/java/com/teamoffroad/feature/auth/presentation/AgreeTermsAndConditionsScreen.kt
+++ feature/auth/src/main/java/com/teamoffroad/feature/auth/presentation/AgreeTermsAndConditionsScreen.kt
@@ -1,16 +1,28 @@
 package com.teamoffroad.feature.auth.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,6 +36,10 @@ import com.teamoffroad.feature.auth.presentation.component.AgreeTermsAndConditio
 import com.teamoffroad.feature.auth.presentation.component.OffroadBasicBtn
 import com.teamoffroad.feature.auth.presentation.model.DialogState
 import com.teamoffroad.offroad.feature.auth.R
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Composable
 internal fun AgreeTermsAndConditionsScreen(
@@ -31,64 +47,105 @@ internal fun AgreeTermsAndConditionsScreen(
     modifier: Modifier = Modifier,
     viewModel: AgreeTermsAndConditionsViewModel = hiltViewModel(),
 ) {
+    val context = LocalContext.current
     val isDialogShown by viewModel.dialogState.collectAsState()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    var snackBarShowState by remember { mutableStateOf(false) }
+    val currentDateTime = remember { LocalDateTime.now() }
+    val formatter = remember {
+        DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm", Locale.getDefault())
+    }
+
+    LaunchedEffect(snackBarShowState) {
+        if (snackBarShowState) {
+            coroutineScope.launch {
+                val snackBar = snackBarHostState.showSnackbar(
+                    message = if (uiState.marketing) "${currentDateTime.format(formatter)}부로 마케팅 정보 수신 동의 처리되었습니다."
+                    else "${currentDateTime.format(formatter)}부로 마케팅 정보 수신 비동의 처리되었습니다.",
+                    actionLabel = "닫기",
+                    duration = SnackbarDuration.Short
+                )
+                when (snackBar) {
+                    SnackbarResult.ActionPerformed -> {}
+                    SnackbarResult.Dismissed -> {}
+                }
+            }
+            snackBarShowState = false
+        }
+    }
     LaunchedEffect(uiState) {
         viewModel.apply {
             updateAgreeTermsAndConditionsUiState()
         }
     }
 
-    Column(
-        Modifier
+    Box(
+        modifier = Modifier
             .navigationPadding()
             .fillMaxSize()
             .background(Main1)
     ) {
-        AgreeTermsAndConditionsTopBar(modifier.padding(bottom = 24.dp))
-        AgreeTermsAndConditionsTopBarAllAgreeBox(
-            isChecked = uiState.serviceUtil && uiState.personalInfo && uiState.location && uiState.marketing,
-            onClick = { viewModel.allCheckedChangedListener() },
-            modifier = Modifier.padding(bottom = 22.dp)
-        )
-        AgreeTermsAndConditionsItem(
-            text = stringResource(R.string.auth_agree_and_terms_conditions_service),
-            isChecked = uiState.serviceUtil,
-            isRequired = true,
-            onClick = { viewModel.serviceCheckedChangedListener() },
-            dialogShown = { viewModel.changeDialogState(DialogState.SERVICE_DIALOG) }
-        )
-        AgreeTermsAndConditionsItem(
-            text = stringResource(R.string.auth_agree_and_terms_conditions_personal),
-            isChecked = uiState.personalInfo,
-            isRequired = true,
-            onClick = { viewModel.personalCheckedChangedListener() },
-            dialogShown = { viewModel.changeDialogState(DialogState.PERSONAL_DIALOG) }
-        )
-        AgreeTermsAndConditionsItem(
-            text = stringResource(R.string.auth_agree_and_terms_conditions_location),
-            isChecked = uiState.location,
-            isRequired = true,
-            onClick = { viewModel.locationCheckedChangedListener() },
-            dialogShown = { viewModel.changeDialogState(DialogState.LOCATION_DIALOG) }
-        )
-        AgreeTermsAndConditionsItem(
-            text = stringResource(R.string.auth_agree_and_terms_conditions_marketing),
-            isChecked = uiState.marketing,
-            isRequired = false,
-            onClick = { viewModel.marketingCheckedChangedListener() },
-            dialogShown = { viewModel.changeDialogState(DialogState.MARKETING_DIALOG) }
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        OffroadBasicBtn(
+        Column {
+            AgreeTermsAndConditionsTopBar(modifier.padding(bottom = 24.dp))
+            AgreeTermsAndConditionsTopBarAllAgreeBox(
+                isChecked = uiState.serviceUtil && uiState.personalInfo && uiState.location && uiState.marketing,
+                onClick = {
+                    viewModel.allCheckedChangedListener()
+                    snackBarShowState = true
+                },
+                modifier = Modifier.padding(bottom = 22.dp)
+            )
+            AgreeTermsAndConditionsItem(
+                text = stringResource(R.string.auth_agree_and_terms_conditions_service),
+                isChecked = uiState.serviceUtil,
+                isRequired = true,
+                onClick = { viewModel.serviceCheckedChangedListener() },
+                dialogShown = { viewModel.changeDialogState(DialogState.SERVICE_DIALOG) }
+            )
+            AgreeTermsAndConditionsItem(
+                text = stringResource(R.string.auth_agree_and_terms_conditions_personal),
+                isChecked = uiState.personalInfo,
+                isRequired = true,
+                onClick = { viewModel.personalCheckedChangedListener() },
+                dialogShown = { viewModel.changeDialogState(DialogState.PERSONAL_DIALOG) }
+            )
+            AgreeTermsAndConditionsItem(
+                text = stringResource(R.string.auth_agree_and_terms_conditions_location),
+                isChecked = uiState.location,
+                isRequired = true,
+                onClick = { viewModel.locationCheckedChangedListener() },
+                dialogShown = { viewModel.changeDialogState(DialogState.LOCATION_DIALOG) }
+            )
+            AgreeTermsAndConditionsItem(
+                text = stringResource(R.string.auth_agree_and_terms_conditions_marketing),
+                isChecked = uiState.marketing,
+                isRequired = false,
+                onClick = {
+                    snackBarShowState = true
+                    viewModel.marketingCheckedChangedListener()
+                },
+                dialogShown = { viewModel.changeDialogState(DialogState.MARKETING_DIALOG) }
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            OffroadBasicBtn(
+                modifier = Modifier
+                    .padding(horizontal = 24.dp)
+                    .padding(bottom = 72.dp)
+                    .height(50.dp),
+                text = stringResource(R.string.auth_basic_button),
+                updateState = { viewModel.changedMarketingAgree(uiState.marketing) },
+                onClick = { navigateToSetNicknameScreen() },
+                isActive = uiState.success,
+            )
+        }
+        SnackbarHost(
             modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .padding(bottom = 72.dp)
-                .height(50.dp),
-            text = stringResource(R.string.auth_basic_button),
-            updateState = { viewModel.changedMarketingAgree(uiState.marketing) },
-            onClick = { navigateToSetNicknameScreen() },
-            isActive = uiState.success,
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(bottom = 30.dp),
+            hostState = snackBarHostState
         )
     }
 
@@ -167,7 +224,17 @@ internal fun AgreeTermsAndConditionsScreen(
                 onDisAgreeClick = { viewModel.marketingDialogCheckedChangedListener(false) },
                 onClickCancel = {
                     viewModel.changeDialogState(DialogState.EMPTY)
+                    snackBarShowState = true
                 })
         }
     }
+}
+
+@Composable
+fun getCurrentDateTime(): String {
+    val currentDateTime = remember { LocalDateTime.now() }
+    val formatter = remember {
+        DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm", Locale.getDefault())
+    }
+    return currentDateTime.format(formatter)
 }
