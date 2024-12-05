1e5ffa3d49904c0fd29dc36db967e1735a79263d
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-12-05 16:17:34 +0900

    refactor: Delete unused code

--- feature/mypage/build.gradle.kts
+++ feature/mypage/build.gradle.kts
@@ -11,7 +11,6 @@ android {
 
 dependencies {
     implementation(project(":feature:auth"))
-
     implementation(libs.retrofit.kotlinx.serialization)
     implementation(libs.lottie.compose)
     implementation(libs.gson)
--- feature/mypage/src/main/java/com/teamoffroad/feature/mypage/presentation/SettingViewModel.kt
+++ feature/mypage/src/main/java/com/teamoffroad/feature/mypage/presentation/SettingViewModel.kt
@@ -3,7 +3,6 @@ package com.teamoffroad.feature.mypage.presentation
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.teamoffroad.core.common.domain.usecase.ClearTokensUseCase
 import com.teamoffroad.core.common.domain.usecase.SetAutoSignInUseCase
 import com.teamoffroad.feature.auth.domain.usecase.UserMarketingAgreeUseCase
@@ -73,13 +72,11 @@ class SettingViewModel @Inject constructor(
             runCatching {
                 clearTokensUseCase()
                 setAutoSignInUseCase.invoke(false)
+            }.onSuccess {
+                _settingUiState.value = _settingUiState.value.copy(reset = true)
+            }.onFailure {
+                it.message.toString()
             }
-                .onSuccess {
-                    _settingUiState.value = _settingUiState.value.copy(reset = true)
-                }
-                .onFailure {
-                    it.message.toString()
-                }
         }
     }
 }
