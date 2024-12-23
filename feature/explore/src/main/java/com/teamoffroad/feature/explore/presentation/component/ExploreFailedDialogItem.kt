c874b9c4870379479ef79738d14a69a94c23d1b9
junseo511 <prime54@g.skku.edu>
2024-12-23 21:25:36 +0900

    refactor: Adjust image size

--- feature/explore/src/main/java/com/teamoffroad/feature/explore/presentation/component/ExploreFailedDialogItem.kt
+++ feature/explore/src/main/java/com/teamoffroad/feature/explore/presentation/component/ExploreFailedDialogItem.kt
@@ -1,19 +1,17 @@
 package com.teamoffroad.feature.explore.presentation.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.teamoffroad.core.designsystem.component.AdaptationImage
-import com.teamoffroad.offroad.feature.explore.R
 
 @Composable
 fun ExploreFailedDialogContent(
@@ -30,20 +28,13 @@ fun ExploreFailedDialogContent(
                 .padding(bottom = 88.dp)
                 .align(Alignment.BottomCenter),
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_explore_fail),
-                contentDescription = "",
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .size(28.dp)
-                    .padding(bottom = 8.dp),
-            )
             AdaptationImage(
                 imageUrl = url,
+                contentScale = ContentScale.FillHeight,
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
                     .padding(start = 10.dp)
-                    .size(96.dp),
+                    .height(166.dp),
             )
         }
     }
