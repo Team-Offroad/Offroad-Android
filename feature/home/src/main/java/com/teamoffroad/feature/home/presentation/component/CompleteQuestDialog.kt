bbe89929301bfd2983918358b9457297733d569d
junseo511 <prime54@g.skku.edu>
2025-03-03 23:45:22 +0900

    refactor: Remove unused import

--- feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/CompleteQuestDialog.kt
+++ feature/home/src/main/java/com/teamoffroad/feature/home/presentation/component/CompleteQuestDialog.kt
@@ -1,7 +1,6 @@
 package com.teamoffroad.feature.home.presentation.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,13 +11,11 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -37,7 +34,7 @@ fun CompleteQuestDialog(
     modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(14.dp),
     backgroundColor: Color = Main3,
-    textColor: Color = Main2
+    textColor: Color = Main2,
 ) {
     Dialog(
         onDismissRequest = { onClickCancel() },
