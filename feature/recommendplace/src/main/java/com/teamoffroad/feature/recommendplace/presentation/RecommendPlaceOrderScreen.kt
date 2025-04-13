93ac9c02384828a882e8a3a5fe9f8c72bf8784eb
YuJeongHyun <0703olivia@naver.com>
2025-04-13 23:33:01 +0900

    feature: Add recommend place order location

--- feature/recommendplace/src/main/java/com/teamoffroad/feature/recommendplace/presentation/RecommendPlaceOrderScreen.kt
+++ feature/recommendplace/src/main/java/com/teamoffroad/feature/recommendplace/presentation/RecommendPlaceOrderScreen.kt
@@ -1,6 +1,5 @@
 package com.teamoffroad.feature.recommendplace.presentation
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -22,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -47,7 +47,10 @@ fun RecommendPlaceOrder(
 ) {
     var selectedType by remember { mutableStateOf<PlaceType?>(null) }
     var showSelectedTypeWarning by remember { mutableStateOf(false) }
+    var locationText by remember { mutableStateOf("") }
+    var showLocationTextWarning by remember { mutableStateOf(false) }
 
+    val focusManager = LocalFocusManager.current
     val showBackConfirmDialog = remember { mutableStateOf(false) }
 
     Column(
@@ -56,9 +59,13 @@ fun RecommendPlaceOrder(
             .background(color = Main1)
             .fillMaxSize()
             .actionBarPadding()
+            .clickableWithoutRipple { focusManager.clearFocus() }
     ) {
         Column {
             RecommendPlaceOrderHeader(
+                selectedType = selectedType,
+                locationText = locationText,
+                showBackConfirmDialogSetter = { showBackConfirmDialog.value = it },
                 showBackConfirmDialog = showBackConfirmDialog,
                 navigateToBack = navigateToBack
             )
@@ -71,19 +78,26 @@ fun RecommendPlaceOrder(
                 },
                 showWarning = showSelectedTypeWarning
             )
-            RecommendPlaceOrderLocation()
+            RecommendPlaceOrderLocation(
+                locationText = locationText,
+                onLocationTextChanged = { locationText = it },
+                showWarning = showLocationTextWarning
+            )
             RecommendPlaceOrderButton(
-                isEnabled = selectedType != null,
+                isEnabled = selectedType != null && locationText.isNotEmpty(),
                 submitOrder = {
                     if (selectedType == null) {
                         showSelectedTypeWarning = true
+                    }
+                    if (locationText.isEmpty()) {
+                        showLocationTextWarning = true
                     }
                 }
             )
         }
     }
 
-    if(showBackConfirmDialog.value) { // 나중에 입력한 경우에만 띄우도록
+    if (showBackConfirmDialog.value) { // 나중에 입력한 경우에만 띄우도록
         RecommendPlaceOrderDialog(
             navigateToBack = navigateToBack,
             onClickCancel = {
@@ -124,6 +138,9 @@ fun RecommendPlaceOrderButton(
 
 @Composable
 fun RecommendPlaceOrderHeader(
+    selectedType: PlaceType?,
+    locationText: String,
+    showBackConfirmDialogSetter: (Boolean) -> Unit,
     showBackConfirmDialog: MutableState<Boolean>,
     navigateToBack: () -> Unit,
 ) {
@@ -141,7 +158,11 @@ fun RecommendPlaceOrderHeader(
                 .padding(start = 6.dp)
                 .size(48.dp)
                 .padding(12.dp)
-                .clickableWithoutRipple { if (true) showBackConfirmDialog.value = true  else navigateToBack() },
+                .clickableWithoutRipple {
+                    if (selectedType != null || locationText.isNotEmpty()) showBackConfirmDialog.value =
+                        true
+                    else navigateToBack()
+                },
         )
 
         Text(
