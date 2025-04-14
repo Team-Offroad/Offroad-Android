438719281e2ebb3bca3568f88aebd743364b3963
YuJeongHyun <0703olivia@naver.com>
2025-04-14 17:14:19 +0900

    feature: Add text on recommend place order etc

--- feature/recommendplace/src/main/java/com/teamoffroad/feature/recommendplace/presentation/component/RecommendPlaceOrderEtc.kt
+++ feature/recommendplace/src/main/java/com/teamoffroad/feature/recommendplace/presentation/component/RecommendPlaceOrderEtc.kt
@@ -12,16 +12,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.teamoffroad.core.designsystem.theme.Gray100
 import com.teamoffroad.core.designsystem.theme.Gray300
+import com.teamoffroad.core.designsystem.theme.Gray400
 import com.teamoffroad.core.designsystem.theme.Main2
 import com.teamoffroad.core.designsystem.theme.Main3
 import com.teamoffroad.core.designsystem.theme.OffroadTheme
@@ -43,32 +46,51 @@ fun RecommendPlaceOrderEtc(
             style = OffroadTheme.typography.textBold,
             color = Main2
         )
-        BasicTextField(
-            value = etcText,
-            onValueChange = onEtcTextChanged,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 12.dp)
-                .heightIn(min = 202.dp)
-                .background(Main3, RoundedCornerShape(6.dp))
-                .border(1.dp, if (isFocused) Main2 else Gray100, RoundedCornerShape(6.dp))
-                .padding(horizontal = 12.dp, vertical = 14.dp),
-            cursorBrush = SolidColor(Main2),
-            interactionSource = interactionSource,
-            maxLines = 7,
-            singleLine = false,
-            decorationBox = { innerTextField ->
-                Box {
-                    if (etcText.isEmpty()) {
-                        Text(
-                            text = stringResource(id = R.string.recommend_place_order_question_etc_placeholder),
-                            style = OffroadTheme.typography.textAuto,
-                            color = Gray300
-                        )
+
+        Box {
+            BasicTextField(
+                value = etcText,
+                onValueChange = onEtcTextChanged,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+                    .heightIn(min = 202.dp)
+                    .background(Main3, RoundedCornerShape(6.dp))
+                    .border(1.dp, if (isFocused) Main2 else Gray100, RoundedCornerShape(6.dp))
+                    .padding(horizontal = 12.dp, vertical = 14.dp),
+                cursorBrush = SolidColor(Main2),
+                interactionSource = interactionSource,
+                maxLines = 7,
+                singleLine = false,
+                decorationBox = { innerTextField ->
+                    Box {
+                        if (etcText.isEmpty()) {
+                            Text(
+                                text = stringResource(id = R.string.recommend_place_order_question_etc_placeholder),
+                                style = OffroadTheme.typography.textAuto,
+                                color = Gray300
+                            )
+                        }
+                        innerTextField()
                     }
-                    innerTextField()
                 }
-            }
-        )
+            )
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = OffroadTheme.typography.hint.toSpanStyle().copy(color = Gray400)
+                    ) {
+                        append("${etcText.length}")
+                    }
+                    append(" / 200")
+                },
+                style = OffroadTheme.typography.hint,
+                color = Gray300,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(bottom = 14.dp, end = 10.dp)
+            )
+
+        }
     }
 }
