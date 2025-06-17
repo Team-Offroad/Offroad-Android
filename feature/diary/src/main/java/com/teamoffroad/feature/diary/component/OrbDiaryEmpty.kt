9ca5ed229239a9bdd7da23cc11e6781bfd4d2880
이석찬 <112953135+leeseokchan00@users.noreply.github.com>
2025-06-18 02:53:04 +0900

    refactor: cherry pick

--- feature/diary/src/main/java/com/teamoffroad/feature/diary/component/OrbDiaryEmpty.kt
+++ feature/diary/src/main/java/com/teamoffroad/feature/diary/component/OrbDiaryEmpty.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,24 +21,33 @@ import com.teamoffroad.core.designsystem.theme.Black55
 import com.teamoffroad.core.designsystem.theme.Main1
 import com.teamoffroad.core.designsystem.theme.Main2
 import com.teamoffroad.core.designsystem.theme.OffroadTheme
-import com.teamoffroad.offroad.feature.diary.R
+import com.teamoffroad.offroad.core.designsystem.R
 
 @Composable
 fun OrbDiaryEmpty(
-    navigateToCharacterChat: (String) -> Unit,
+    characterName: String,
+    navigateToCharacterChat: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val emptyCharacterImage = when (characterName) {
+        "루미" -> R.drawable.img_diary_empty_rumi
+        "레디" -> R.drawable.img_diary_empty_ready
+        else -> R.drawable.img_diary_empty_nova
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxWidth(),
     ) {
         Image(
-            painter = painterResource(id = R.drawable.img_diary_empty),
+            painter = painterResource(id = emptyCharacterImage),
             contentDescription = "empty",
-            modifier = Modifier.padding(bottom = 25.dp),
+            modifier = Modifier
+                .size(210.dp)
+                .padding(bottom = 25.dp),
         )
         Text(
-            text = stringResource(id = R.string.diary_empty_content),
+            text = stringResource(id = com.teamoffroad.offroad.feature.diary.R.string.diary_empty_content),
             textAlign = TextAlign.Center,
             color = Black55.copy(alpha = 0.55f),
             style = OffroadTheme.typography.boxMedi,
@@ -51,10 +61,10 @@ fun OrbDiaryEmpty(
                     shape = RoundedCornerShape(46.dp)
                 )
                 .padding(horizontal = 68.dp, vertical = 14.dp)
-                .clickableWithoutRipple { navigateToCharacterChat("???") },
+                .clickableWithoutRipple { navigateToCharacterChat() },
         ) {
             Text(
-                text = stringResource(id = R.string.diary_empty_go_chatting),
+                text = stringResource(id = com.teamoffroad.offroad.feature.diary.R.string.diary_empty_go_chatting),
                 color = Main1,
                 style = OffroadTheme.typography.btnSmall,
             )
