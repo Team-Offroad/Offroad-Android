861ca464651dcdc7961ba6724491268ada765be1
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-06-18 02:00:46 +0900

    refactor: Edit memoryLight color

--- feature/diary/src/main/java/com/teamoffroad/feature/diary/presentation/DiaryScreen.kt
+++ feature/diary/src/main/java/com/teamoffroad/feature/diary/presentation/DiaryScreen.kt
@@ -1,6 +1,5 @@
 package com.teamoffroad.feature.diary.presentation
 
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
