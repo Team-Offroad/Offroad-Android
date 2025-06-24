c5f7af6652ff652f6623cb512cc73e640d771113
junseo511 <prime54@g.skku.edu>
2025-06-24 18:09:31 +0900

    refactor: Update centerLocation logic in CourseQuestPlacesUiModel

--- feature/explore/src/main/java/com/teamoffroad/feature/explore/presentation/model/CourseQuestPlacesUiModel.kt
+++ feature/explore/src/main/java/com/teamoffroad/feature/explore/presentation/model/CourseQuestPlacesUiModel.kt
@@ -1,16 +1,25 @@
 package com.teamoffroad.feature.explore.presentation.model
 
 import com.teamoffroad.feature.explore.domain.model.Location
+import com.teamoffroad.feature.explore.domain.model.Location.Companion.toLatLng
 
 data class CourseQuestPlacesUiModel(
     val places: List<CourseQuestPlaceUiModel> = emptyList(),
 ) {
     val centerLocation: Location
-        get() =
-            Location(
-                latitude = places.map { it.position.latitude }.average(),
-                longitude = places.map { it.position.longitude }.average(),
-            )
+        get() {
+            val averageLatitude = places.map { it.position.latitude }.average()
+            val averageLongitude = places.map { it.position.longitude }.average()
+
+            val averageLocation = Location(latitude = averageLatitude, longitude = averageLongitude).toLatLng()
+
+            val closestPlace =
+                places.minByOrNull { place ->
+                    place.position.toLatLng().distanceTo(averageLocation)
+                }
+
+            return closestPlace?.position ?: Location()
+        }
 
     val isComplete: Boolean
         get() = places.all { it.isVisited }
